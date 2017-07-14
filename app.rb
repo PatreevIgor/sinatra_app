4db5a3826a819e7b2faa16ec914ca4c5094a1fa4
PatreewIgor <patreewigor@yandex.ru>
2017-07-14 18:31:22 +0300

    Add partials. Fix autorization.

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ require './models/user'
 require 'oauth2'
 require 'json'
 require 'dotenv'
+require 'sinatra/partial'
 Dotenv.load
-
+register Sinatra::Partial
+enable :partial_underscores
+set :partial_template_engine, :erb
 enable :sessions
 
-# Scopes are space separated strings
-SCOPES = [
-    'https://www.googleapis.com/auth/userinfo.email'
-].join(' ')
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email'].join(' ')
 
 unless G_API_CLIENT = ENV['OAUTH2_CLIENT_ID']
   raise "You must specify the G_API_CLIENT env variable"
@@ -22,21 +22,9 @@ unless G_API_SECRET = ENV['OAUTH2_CLIENT_SECRET']
   raise "You must specify the G_API_SECRET env veriable"
 end
 
-def client
-  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
-                :site => 'https://accounts.google.com',
-                :authorize_url => "/o/oauth2/auth",
-                :token_url => "/o/oauth2/token"
-              })
-end
-
 get '/' do
-  erb :index
-end
-
-get '/users' do
   @users = User.all
-  erb :users
+  erb :index
 end
 
 get "/auth" do
@@ -46,18 +34,37 @@ end
 get '/oauth2callback' do
   access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
   session[:access_token] = access_token.token
-  @message = "Successfully authenticated with the server"
-  @access_token = session[:access_token]
-
-  # parsed is a handy method on an OAuth2::Response object that will
-  # intelligently try and parse the response.body
-  @email = access_token.get('https://www.googleapis.com/userinfo/email?alt=json').parsed
-  erb :success
+  session[:email] = access_token.get('https://www.googleapis.com/userinfo/email?alt=json').parsed
+  redirect '/'
 end
 
+get '/logout' do
+  session[:access_token] = nil
+  redirect '/'
+end
+
+post '/create' do
+  User.create(first_name: params[:username])
+  redirect '/'
+end
+
+post '/delete' do
+  User.last.delete
+  redirect '/'
+end
+
+private
 def redirect_uri
   uri = URI.parse(request.url)
   uri.path = '/oauth2callback'
   uri.query = nil
   uri.to_s
+end
+
+def client
+  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
+                :site => 'https://accounts.google.com',
+                :authorize_url => "/o/oauth2/auth",
+                :token_url => "/o/oauth2/token"
+              })
 end
