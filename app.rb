1d6b9f3e6e32f1d69cd90cc2c5c4ea95b9e3dee8
PatreewIgor <patreewigor@yandex.ru>
2017-07-18 12:21:00 +0300

    Refactoring

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require 'oauth2'
 require 'json'
 require 'dotenv'
 require 'sinatra/partial'
+require 'jwt'
+
 Dotenv.load
 register Sinatra::Partial
 enable :partial_underscores
@@ -14,21 +16,52 @@ enable :sessions
 
 SCOPES = ['https://www.googleapis.com/auth/userinfo.email'].join(' ')
 
-unless G_API_CLIENT = ENV['OAUTH2_CLIENT_ID']
-  raise "You must specify the G_API_CLIENT env variable"
+# --------------------------------------------------------------------
+
+signing_key_path = File.expand_path("../app.rsa", __FILE__)
+verify_key_path = File.expand_path("../app.rsa.pub", __FILE__)
+
+signing_key = ""
+verify_key = ""
+
+File.open(signing_key_path) do |file|
+  signing_key = OpenSSL::PKey.read(file)
 end
 
-unless G_API_SECRET = ENV['OAUTH2_CLIENT_SECRET']
-  raise "You must specify the G_API_SECRET env veriable"
+File.open(verify_key_path) do |file|
+  verify_key = OpenSSL::PKey.read(file)
 end
+
+set :signing_key, signing_key
+set :verify_key, verify_key
+
+# --------------------------------------------------------------------
+
+def check_g_api_client
+  raise 'You must specify the G_API_CLIENT env variable' unless ENV['OAUTH2_CLIENT_ID']
+end
+
+def check_g_api_secret
+  raise 'You must specify the G_API_SECRET env veriable' unless ENV['OAUTH2_CLIENT_SECRET']
+end
+
+check_g_api_client
+check_g_api_secret
+
+# --------------------------------------------------------------------
+
 
 get '/' do
   @users = User.all
+  if session[:access_token]
+    @encoded_token = JWT.encode(session[:access_token], settings.signing_key, "RS256")
+    @decoded_token = JWT.decode(@encoded_token, settings.verify_key, true)
+  end
   erb :index
 end
 
-get "/auth" do
-  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+get '/auth' do
+  redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => 'offline')
 end
 
 get '/oauth2callback' do
@@ -67,7 +100,7 @@ def redirect_uri
 end
 
 def client
-  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
+  client ||= OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], {
                 :site => 'https://accounts.google.com',
                 :authorize_url => "/o/oauth2/auth",
                 :token_url => "/o/oauth2/token"
